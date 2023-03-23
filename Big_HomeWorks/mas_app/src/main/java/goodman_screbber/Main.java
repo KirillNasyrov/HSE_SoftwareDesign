e8fbc0c8d9238314ecc6d7fbf8f75889c088ac5c
Kirill Nasyrov <90870970+KirillNasyrov@users.noreply.github.com>
2023-03-23 21:41:26 +0300

    test

--- Big_HomeWorks/mas_app/src/main/java/goodman_screbber/Main.java
+++ Big_HomeWorks/mas_app/src/main/java/goodman_screbber/Main.java
@@ -8,7 +8,18 @@ import model.MenuItem;
 public class Main {
     public static void main(String[] args) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
-        String jsonString = "";
+        String jsonString = """
+                {
+                            "menu_dishes": [{
+                                                    "menu_dish_id": 12,
+                                                    "menu_dish_card": 518,
+                                                    "menu_dish_price": 59,
+                                                    "menu_dish_active": true
+                                        }
+                            ]
+                }
+                                
+                """;
 
         Menu menuItem = objectMapper.readValue(jsonString, Menu.class);
         for (MenuItem menuDish : menuItem.menu_dishes) {
