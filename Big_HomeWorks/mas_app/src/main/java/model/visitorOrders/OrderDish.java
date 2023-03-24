32191830413067ee829ee406d28299d6f6b0366a
Kirill Nasyrov <90870970+KirillNasyrov@users.noreply.github.com>
2023-03-24 13:18:55 +0300

    add VisitorOrder

---
+++ Big_HomeWorks/mas_app/src/main/java/model/visitorOrders/OrderDish.java
@@ -0,0 +1,30 @@
+package model.visitorOrders;
+
+public class OrderDish {
+    private Integer ord_dish_id;
+    private Integer menu_dish;
+
+    public OrderDish(Integer ordDishId, Integer menuDish) {
+        ord_dish_id = ordDishId;
+        menu_dish = menuDish;
+    }
+
+    public OrderDish() {
+    }
+
+    public Integer getOrd_dish_id() {
+        return ord_dish_id;
+    }
+
+    public void setOrd_dish_id(Integer ord_dish_id) {
+        this.ord_dish_id = ord_dish_id;
+    }
+
+    public Integer getMenu_dish() {
+        return menu_dish;
+    }
+
+    public void setMenu_dish(Integer menu_dish) {
+        this.menu_dish = menu_dish;
+    }
+}
---
+++ Big_HomeWorks/mas_app/src/main/java/model/visitorOrders/VisitorOrder.java
@@ -0,0 +1,64 @@
+package model.visitorOrders;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class VisitorOrder {
+    private String vis_name;
+    private Date vis_ord_started;
+    private Date vis_ord_ended;
+    private Integer vis_ord_total;
+    private List<OrderDish> vis_ord_dishes;
+
+    public VisitorOrder(String vis_name, Date vis_ord_started, Date vis_ord_ended, Integer vis_ord_total, List<OrderDish> vis_ord_dishes) {
+        this.vis_name = vis_name;
+        this.vis_ord_started = vis_ord_started;
+        this.vis_ord_ended = vis_ord_ended;
+        this.vis_ord_total = vis_ord_total;
+        this.vis_ord_dishes = new ArrayList<>(vis_ord_dishes);
+    }
+
+    public VisitorOrder() {
+    }
+
+    public String getVis_name() {
+        return vis_name;
+    }
+
+    public void setVis_name(String vis_name) {
+        this.vis_name = vis_name;
+    }
+
+    public Date getVis_ord_started() {
+        return vis_ord_started;
+    }
+
+    public void setVis_ord_started(Date vis_ord_started) {
+        this.vis_ord_started = vis_ord_started;
+    }
+
+    public Date getVis_ord_ended() {
+        return vis_ord_ended;
+    }
+
+    public void setVis_ord_ended(Date vis_ord_ended) {
+        this.vis_ord_ended = vis_ord_ended;
+    }
+
+    public Integer getVis_ord_total() {
+        return vis_ord_total;
+    }
+
+    public void setVis_ord_total(Integer vis_ord_total) {
+        this.vis_ord_total = vis_ord_total;
+    }
+
+    public List<OrderDish> getVis_ord_dishes() {
+        return new ArrayList<>(vis_ord_dishes);
+    }
+
+    public void setVis_ord_dishes(List<OrderDish> vis_ord_dishes) {
+        this.vis_ord_dishes = new ArrayList<>(vis_ord_dishes);
+    }
+}
